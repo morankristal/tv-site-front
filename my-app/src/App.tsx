272e640473884158c19403d35949142238bdbbee
מורן קריסטל <morank44@gmail.com>
2024-10-15 21:38:38 +0300

    init

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {Button} from "@mantine/core";
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MantineProvider>
+      <div>
+         <Button variant="light" color="pink" size="xl" radius="xl">Button</Button>
+          <div className="bg-red-500 p-4"> moran kristal </div>
+      </div>
+    </MantineProvider>
+  )
 }
 
-export default App;
+export default App
